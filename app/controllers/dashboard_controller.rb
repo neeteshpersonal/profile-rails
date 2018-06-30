f7dadba1948f3914816411cbe5ee9a83317976b6
Neetesh gupta <neetesh.gupta95@gmail.com>
2018-07-01 01:14:27 +0530

    Download Resume.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,13 @@
 class DashboardController < ApplicationController
-	def home
+	def index
 		
+	end
+
+	def resume_download
+		send_file(
+	    "#{Rails.root}/public/NeeteshGupta.pdf",
+	    filename: "NeeteshGupta.pdf",
+	    type: "application/pdf"
+	  )
 	end
 end
