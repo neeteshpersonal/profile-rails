0627cad41b13bfb94b793c244a6b70cb199ca434
Neetesh gupta <neetesh.gupta95@gmail.com>
2018-07-01 01:13:32 +0530

    Image cleanup and pdf file added, routes changes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/home', to: "dashboard#home"
+  resources :dashboard, only: [:index]
+  # get '/', to: "dashboard#index"
+  get "/download_resume", to: "dashboard#resume_download"
 
-  root "dashboard#home"
+  root "dashboard#index"
 end
